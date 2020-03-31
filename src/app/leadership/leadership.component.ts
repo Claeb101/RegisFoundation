58b0767dbf2f759afd9f6586fda49b63af1c908b
Caleb Arulandu <calebarulandu@gmail.com>
2020-03-30 21:37:00 -0400

    Working team transitions

--- src/app/leadership/leadership.component.ts
+++ src/app/leadership/leadership.component.ts
@@ -14,17 +14,20 @@ export class LeadershipComponent implements OnInit {
   constructor() {
     let des = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna ac placerat vestibulum lectus mauris. Eget velit aliquet sagittis id consectetur purus. Feugiat nisl pretium fusce id velit ut tortor pretium. Rhoncus mattis rhoncus urna neque viverra.";
     let img = "assets/person.jpg";
+    let name = "Jovina Arulandu";
+    let role = "Big Boss"
+
     this.title = "Hello There";
     this.trustees = [
-      {image_path: img, description: des},
-      {image_path: img, description: des},
-      {image_path: img, description: des},
+      {image_path: img, description: des, name: name, role: role},
+      {image_path: img, description: des, name: name, role: role},
+      {image_path: img, description: des, name: name, role: role},
     ];
 
     this.mentors = [
-      {image_path: img, description: des},
-      {image_path: img, description: des},
-      {image_path: img, description: des},
+      {image_path: img, description: des, name: name, role: role},
+      {image_path: img, description: des, name: name, role: role},
+      {image_path: img, description: des, name: name, role: role},
     ];
   }
 
--- src/app/leadership/teamMember.ts
+++ src/app/leadership/teamMember.ts
@@ -1,8 +1,13 @@
 export class TeamMember {
     image_path : string;
     description : string;
-    constructor(image_path, description){
+    name : string;
+    role : string;
+
+    constructor(image_path:string, description:string, name:string, role:string){
         this.image_path = image_path;
         this.description = description;
+        this.name = name;
+        this.role = role;
     }
 }
