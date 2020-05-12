d19b110a664fdef4b7121ae998abe5ae4e788fba
Caleb Arulandu <calebarulandu@gmail.com>
2020-05-12 16:50:49 -0400

    Routing Recofig Test Fix1

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -3,17 +3,18 @@ import { AboutComponent } from './about/about.component';
 import { HelpComponent } from './help/help.component';
 import { DonateComponent } from './donate/donate.component';
 import { HomeComponent } from './home/home.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: './home', component: HomeComponent},
-  { path: './donate', component: DonateComponent},
-  { path: './about', component: AboutComponent},
-  { path: './help', component: HelpComponent},
-  { path: './leadership', component: LeadershipComponent},
-  { path: '**', redirectTo: './home' }
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'donate', component: DonateComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'help', component: HelpComponent},
+  { path: 'leadership', component: LeadershipComponent},
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
