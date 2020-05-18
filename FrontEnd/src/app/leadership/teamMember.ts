fbd23d2c2063365ed7dca91abc27f779928e0852
Caleb Arulandu <calebarulandu@gmail.com>
2020-05-18 13:35:00 -0400

    Leadership Toggle Description

--- FrontEnd/src/app/leadership/teamMember.ts
+++ FrontEnd/src/app/leadership/teamMember.ts
@@ -4,10 +4,13 @@ export class TeamMember {
     name : string;
     role : string;
 
-    constructor(image_path:string, description:string, name:string, role:string){
+    isRevealed : boolean;
+
+    constructor(image_path:string, description:string, name:string, role:string, reveal:boolean){
         this.image_path = image_path;
         this.description = description;
         this.name = name;
         this.role = role;
+        this.isRevealed = false;
     }
 }
