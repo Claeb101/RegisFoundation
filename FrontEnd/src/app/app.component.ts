1c1e6f65fbad1fb7d6cb1dc734b3d32c6e061c91
Caleb Arulandu <calebarulandu@gmail.com>
2020-05-21 22:54:21 -0400

    Google Analytics

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import * as AOS from 'aos';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -8,7 +12,16 @@ import * as AOS from 'aos';
 })
 export class AppComponent {
   title = 'Regis';
-
+  constructor(router: Router) {
+    const navEndEvents = router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+    );
+    navEndEvents.subscribe((event : NavigationEnd) => { 
+      gtag('config', 'UA-167359120-1', {
+        'page_path': event.urlAfterRedirects
+      });
+    })
+  }
   ngOnInit(): void {
     AOS.init();
   }
